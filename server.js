0277744707361a7cc98e7fef63c0a147e67c081d
Zandolor <steven.j.jordaan2000@gmail.com>
2021-05-13 22:43:23 +0200

    Add cors configuration for UI

--- server.js
+++ server.js
@@ -5,6 +5,19 @@ const app = express();
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
+// Configure to allow UI to call api
+app.use((req, res, next) => {
+  res.header(
+    'Access-Control-Allow-Origin',
+    'https://game-engine-ui.herokuapp.com/'
+  );
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
 // set the home page route
 app.get('/', (req, res) => {
   // ejs render automatically looks in the views folder
