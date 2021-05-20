39928638c8d2e3e690e0aa7dfdc7f974c0d11dd9
Xeno Rhoda <xeno@bbd.co.za>
2021-05-20 12:12:12 +0200

    changed verify to a fuction

--- app/middleware/verifySignup.js
+++ app/middleware/verifySignup.js
@@ -2,17 +2,19 @@ const db = require("../models");
 const User = db.user;
 
 checkDuplicateUsername = (req, res, next) => {
-  User.findOne({ where: { username: req.body.username } }).then((user) => {
-    if (user) {
-      res.status(400).send({ message: "Failed! Username is already in use!" });
-      return;
-    }
-    next();
-  });
+	User.findOne({ where: { username: req.body.username } }).then((user) => {
+		if (user) {
+			res.status(400).send({
+				message: "Failed! Username is already in use!",
+			});
+			return;
+		}
+		next();
+	});
 };
 
-const verifySignUp = {
-  checkDuplicateUsername: checkDuplicateUsername,
-};
+const verifySignUp = () => ({
+	checkDuplicateUsername: checkDuplicateUsername,
+});
 
 module.exports = verifySignUp;
