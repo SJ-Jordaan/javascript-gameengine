2c1e8b431fe88d7ba271cb1201e76b18b0fbe0de
SJ-Jordaan <u18074848@tuks.co.za>
2021-05-20 12:40:39 +0200

    Fix require issue

--- app/middleware/index.js
+++ app/middleware/index.js
@@ -1,7 +1,7 @@
 const authJwt = require("./authJwt");
-const verifySignUp = require("./verifySignUp");
+const verifySignup = require("./verifySignup");
 
 module.exports = {
   authJwt,
-  verifySignUp,
+  verifySignup,
 };
