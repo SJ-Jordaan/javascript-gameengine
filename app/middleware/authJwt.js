fb5fcc2b23197df7579fd20774ff6615085f1d8f
SJ-Jordaan <u18074848@tuks.co.za>
2021-05-20 13:02:57 +0200

    Add better logging to isValidUid

--- app/middleware/authJwt.js
+++ app/middleware/authJwt.js
@@ -37,7 +37,12 @@ isValidUID = (req, res, next) => {
       next();
     })
     .catch((error) => {
-      res.status(403).send({ message: "User does not exist" });
+      res
+        .status(403)
+        .send({
+          message: "User does not exist or see error for more details",
+          error: error,
+        });
     });
 };
 
