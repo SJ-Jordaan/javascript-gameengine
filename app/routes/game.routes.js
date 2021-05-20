d5ec4d70eef4a7f42d8d0f2b551ddcd1cd7ee51d
Xeno Rhoda <xeno@bbd.co.za>
2021-05-20 18:07:11 +0200

    Added cors

--- app/routes/game.routes.js
+++ app/routes/game.routes.js
@@ -4,6 +4,8 @@ const router = require("express").Router();
 
 module.exports = (app) => {
   app.use((req, res, next) => {
+  
+    res.header("Access-Control-Allow-Origin", "*");
     res.header(
       "Access-Control-Allow-Headers",
       "x-access-token, Origin, Content-Type, Accept"
